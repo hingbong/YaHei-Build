d0c6074905409f8192ef020e4c2606b5d44192d8
hingbong💫 <hingbonglo@gmail.com>
2022-09-04 11:21:57 +0800

    Judou sans to build YAHEI, I.Ming to build simsun

--- 02_to_classic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-chmod +x SourceHanToClassic/main/otfcc/*
-mkdir out
-cd SourceHanToClassic/main 
-ls ../../src/ | xargs -I{} python3 sourcehantocl.py ../../src/{} ../../out/{} n 1 1 3 2
-cd ../..
