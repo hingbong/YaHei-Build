fadf49fc0f9cb4f1257d850931fb739179018222
hingbong💫 <hingbonglo@gmail.com>
2023-06-01 00:53:42 +0800

    reduce fonts

--- process.ts
+++ process.ts
@@ -3,13 +3,22 @@ import {listDir, runProcess, toWinFontsToolPath} from "./utils.ts";
 const cwd = Deno.cwd()
 const sansFonts = await listDir("sans")
 console.log("sans fonts: ", sansFonts)
-for (const font: string of sansFonts ) {
-    console.log(`to build ${font}`)
-    await runProcess(["python", toWinFontsToolPath, "-i", font, "-tg", "allsans", "-d", `${cwd}/out`, "-r"])
+
+const sansTg = ["msyh", "msjh", "simhei", "msgothic", "meiryo"]
+
+const serifTg = ["simsun", "msmincho", "mingliu"]
+
+for (const font: string of sansFonts) {
+    for (const tg of sansTg) {
+        console.log(`to build ${font} for ${tg}`)
+        await runProcess(["python", toWinFontsToolPath, "-i", font, "-tg", tg, "-d", `${cwd}/out`, "-r"])
+    }
 }
 const serifFonts = await listDir("serif")
 console.log("serif fonts: ", serifFonts)
 for (const font: string of serifFonts) {
-    console.log(`to build ${font}`)
-    await runProcess(["python", toWinFontsToolPath, "-i", font, "-tg", "allserif", "-d", `${cwd}/out`, "-r"])
+    for (const tg of serifTg) {
+        console.log(`to build ${font} for ${tg}`)
+        await runProcess(["python", toWinFontsToolPath, "-i", font, "-tg", tg, "-d", `${cwd}/out`, "-r"])
+    }
 }
